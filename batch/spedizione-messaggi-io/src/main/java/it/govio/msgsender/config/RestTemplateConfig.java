94d61dc867148936d5819f9910d59e2c86222459
Lorenzo Nardi <nardi@link.it>
2022-11-02 09:08:41 +0100

    Corrette richieste ad IO per escludere null values
    - Impostati component scan con i component generati da openapigen
    - Configurato resttemplate con serializzazione dei valori non-null

---
+++ batch/spedizione-messaggi-io/src/main/java/it/govio/msgsender/config/RestTemplateConfig.java
@@ -0,0 +1,39 @@
+package it.govio.msgsender.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Configuration
+public class RestTemplateConfig {
+
+	@Bean
+	RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		// This allows us to read the response more than once - Necessary for debugging.
+		restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(restTemplate.getRequestFactory()));
+
+		// disable default URL encoding
+		DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
+		uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+		restTemplate.setUriTemplateHandler(uriBuilderFactory);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(Include.NON_NULL);
+
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+		messageConverter.setObjectMapper(objectMapper);
+		
+		restTemplate.getMessageConverters().removeIf(m -> m.getClass().getName().equals(MappingJackson2HttpMessageConverter.class.getName()));
+		restTemplate.getMessageConverters().add(messageConverter);
+
+		return restTemplate;
+	}
+}
