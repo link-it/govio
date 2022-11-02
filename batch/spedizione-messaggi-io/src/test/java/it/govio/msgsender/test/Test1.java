786ba82b9c79b859035de931bff668264c50270e
Lorenzo Nardi <nardi@link.it>
2022-11-02 10:12:52 +0100

    Rimosso codice commentato o inutile

--- batch/spedizione-messaggi-io/src/test/java/it/govio/msgsender/test/Test1.java
+++ batch/spedizione-messaggi-io/src/test/java/it/govio/msgsender/test/Test1.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -18,15 +17,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import it.govio.msgsender.Application;
-import it.govio.msgsender.entity.GovioMessagesEntity;
-import it.govio.msgsender.repository.GovioMessagesRepository;
 import it.pagopa.io.v1.api.DefaultApi;
 import it.pagopa.io.v1.api.beans.CreatedMessage;
 import it.pagopa.io.v1.api.beans.LimitedProfile;
@@ -92,21 +88,4 @@ public class Test1 {
 		assertEquals("You are not allowed to issue requests for the recipient.", readValue.getDetail());
 	}
 	
-//	@Test
-//	void SendMessageApiKeyNotAuthorizedTest(){
-//		
-//		backendIOClient.getApiClient().setApiKey("1234567890abcdef1234567890abcdef");
-//		backendIOClient.getApiClient().setDebugging(true);
-//
-//		NewMessage message = new NewMessage();
-//		message.setFiscalCode("AAAAAA00A00A000A");
-//		MessageContent mc = new MessageContent();
-//		mc.setMarkdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mauris lorem, euismod quis sapien ac, pretium ornare augue. Nullam pulvinar ultricies nisi suscipit pulvinar. Maecenas dictum tortor vehicula ante accumsan, sed tempus quam mollis. Morbi dictum purus risus, vel aliquet mauris pretium at. Mauris purus odio, iaculis non metus eu, iaculis interdum dui. Phasellus et hendrerit magna. Ut vehicula turpis in elit dapibus euismod. Cras imperdiet pulvinar diam. Fusce semper massa diam, eu sagittis magna tincidunt quis. Mauris sit amet dui suscipit, volutpat urna a, molestie elit. Mauris in erat a justo rhoncus scelerisque.");
-//		mc.setSubject("Lorem ipsum dolor sit amet");
-//		message.setContent(mc);
-//		CreatedMessage submitMessageforUserWithFiscalCodeInBody = backendIOClient.submitMessageforUserWithFiscalCodeInBody(message);
-//		assertNotNull(submitMessageforUserWithFiscalCodeInBody.getId());
-//	}
-	
-
 }
