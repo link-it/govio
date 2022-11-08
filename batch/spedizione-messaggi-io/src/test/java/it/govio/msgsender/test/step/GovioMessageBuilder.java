44cf13c9b67590a025f80ffdf167665991d81502
Lorenzo Nardi <nardi@link.it>
2022-11-08 10:49:36 +0100

    Aggiunta classe mancante

---
+++ batch/spedizione-messaggi-io/src/test/java/it/govio/msgsender/test/step/GovioMessageBuilder.java
@@ -0,0 +1,35 @@
+package it.govio.msgsender.test.step;
+
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+
+import it.govio.msgsender.entity.GovioMessageEntity;
+import it.govio.msgsender.entity.GovioServiceInstanceEntity;
+import it.govio.msgsender.entity.GovioMessageEntity.GovioMessageEntityBuilder;
+import it.govio.msgsender.entity.GovioMessageEntity.Status;
+import it.pagopa.io.v1.api.beans.Payee;
+
+public class GovioMessageBuilder {
+	
+	public GovioMessageEntity buildGovioMessageEntity(GovioServiceInstanceEntity serviceInstanceEntity, Status status, boolean due_date, Integer amount, String noticeNumber, boolean invalidAfterDueDate, Payee payee, String email) throws URISyntaxException {
+		GovioMessageEntityBuilder messageEntity = GovioMessageEntity.builder()
+				.govioServiceInstance(serviceInstanceEntity)
+				.markdown("Lorem Ipsum")
+				.subject("Subject")
+				.taxcode("AAAAAA00A00A000A")
+				.status(status)
+				.creationDate(LocalDateTime.now())
+				.scheduledExpeditionDate(LocalDateTime.now());
+		if (due_date) messageEntity.due_date(LocalDateTime.now().plusDays(3));
+		if (amount != null && amount > 0) {
+			messageEntity.amount(amount);
+			messageEntity.noticeNumber(noticeNumber);
+			messageEntity.invalidAfterDueDate(invalidAfterDueDate);
+		}
+		if (payee != null) messageEntity.payee(payee.getFiscalCode());
+		if (email != null) messageEntity.email(email);
+		GovioMessageEntity message = messageEntity.build();
+		return message;
+	}
+
+}
