40941d2cb2ffc314473b6b5e49aae3803b4c69d8
Lorenzo Nardi <nardi@link.it>
2022-11-16 17:54:38 +0100

    wip

--- batch/spedizione-messaggi-io/src/main/java/it/govio/msgsender/config/BatchConfig.java
+++ batch/spedizione-messaggi-io/src/main/java/it/govio/msgsender/config/BatchConfig.java
@@ -2,10 +2,9 @@ package it.govio.msgsender.config;
 
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Future;
 
 import javax.persistence.EntityManager;
 
@@ -16,8 +15,9 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +25,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.data.domain.Sort;
 
 import it.govio.msgsender.entity.GovioMessageEntity;
 import it.govio.msgsender.entity.GovioMessageEntity.Status;
 import it.govio.msgsender.repository.GovioMessagesRepository;
 import it.govio.msgsender.step.GetProfileProcessor;
+import it.govio.msgsender.step.GovioMessageAbstractProcessor;
 import it.govio.msgsender.step.NewMessageProcessor;
 
 
@@ -43,9 +43,6 @@ public class BatchConfig  {
 
 	@Autowired
 	private StepBuilderFactory steps;
-	
-	@Autowired
-	JobRepository jobRepository;
 	
 	@Autowired
 	private GetProfileProcessor getProfileProcessor;
@@ -66,42 +63,40 @@ public class BatchConfig  {
 	
 	public Step getProfileStep(){
 		return steps.get("getProfileStep")
-//				.<GovioMessageEntity, Future<GovioMessageEntity>>chunk(10)
-//				.reader(expiredScheduledDateMessageCursor(Status.SCHEDULED))
-//				.processor(asyncProcessor(this.getProfileProcessor))
-//				.writer(asyncMessageWriter())
-				.<GovioMessageEntity, GovioMessageEntity>chunk(10)
-				.reader(expiredScheduledDateMessageReader(Status.SCHEDULED))
-				.processor(this.getProfileProcessor)
-				.writer(messageWriter())
+				.<GovioMessageEntity, Future<GovioMessageEntity>>chunk(10)
+				.reader(expiredScheduledDateMessageCursor(Status.SCHEDULED))
+				.processor(asyncProcessor(this.getProfileProcessor))
+				.writer(asyncMessageWriter())
+				.faultTolerant()
+				.skip(Throwable.class)
+				.skipLimit(Integer.MAX_VALUE)
 				.build();
 	}
 	
-//	private AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncProcessor(ItemProcessor<GovioMessageEntity, GovioMessageEntity> itemProcessor) {
-//		    AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncItemProcessor = new AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity>();
-//		    asyncItemProcessor.setTaskExecutor(taskExecutor());
-//		    asyncItemProcessor.setDelegate(itemProcessor);
-//		    return asyncItemProcessor;
-//	}
+	private AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncProcessor(GovioMessageAbstractProcessor itemProcessor) {
+		    AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncItemProcessor = new AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity>();
+		    asyncItemProcessor.setTaskExecutor(taskExecutor());
+		    asyncItemProcessor.setDelegate(itemProcessor);
+		    return asyncItemProcessor;
+	}
 
 	public Step newMessageStep(){
 		return steps.get("newMessageStep")
-//				.<GovioMessageEntity, Future<GovioMessageEntity>>chunk(10)
-//				.reader(expiredScheduledDateMessageCursor(Status.RECIPIENT_ALLOWED))
-//				.processor(asyncProcessor(this.newMessageProcessor))
-//				.writer(asyncMessageWriter())
-				.<GovioMessageEntity, GovioMessageEntity>chunk(10)
+				.<GovioMessageEntity, Future<GovioMessageEntity>>chunk(10)
 				.reader(expiredScheduledDateMessageCursor(Status.RECIPIENT_ALLOWED))
-				.processor(this.newMessageProcessor)
-				.writer(messageWriter())
+				.processor(asyncProcessor(this.newMessageProcessor))
+				.writer(asyncMessageWriter())
+				.faultTolerant()
+				.skip(Throwable.class)
+				.skipLimit(Integer.MAX_VALUE)
 				.build();
 	}
 	
-//	private AsyncItemWriter<GovioMessageEntity> asyncMessageWriter(){
-//		AsyncItemWriter<GovioMessageEntity> asyncItemWriter = new AsyncItemWriter<GovioMessageEntity>();
-//	    asyncItemWriter.setDelegate(messageWriter());
-//	    return asyncItemWriter;
-//	}
+	private AsyncItemWriter<GovioMessageEntity> asyncMessageWriter(){
+		AsyncItemWriter<GovioMessageEntity> asyncItemWriter = new AsyncItemWriter<GovioMessageEntity>();
+	    asyncItemWriter.setDelegate(messageWriter());
+	    return asyncItemWriter;
+	}
 	
     private RepositoryItemWriter<GovioMessageEntity> messageWriter() {
         final RepositoryItemWriter<GovioMessageEntity> repositoryItemWriter = new RepositoryItemWriter<>();
@@ -110,22 +105,6 @@ public class BatchConfig  {
         return repositoryItemWriter;
     }
 	
-    private RepositoryItemReader<GovioMessageEntity> expiredScheduledDateMessageReader(Status status) {
-        final RepositoryItemReader<GovioMessageEntity> repositoryItemReader = new RepositoryItemReader<>();
-        repositoryItemReader.setRepository(govioMessagesRepository);
-        repositoryItemReader.setMethodName("findAllByStatusAndScheduledExpeditionDateBefore");
-        List<Object> methodArgs = new ArrayList<Object>();
-        methodArgs.add(status);
-        methodArgs.add(LocalDateTime.now());
-        repositoryItemReader.setArguments(methodArgs);
-		final HashMap<String, Sort.Direction> sorts = new HashMap<>();
-		sorts.put("scheduledExpeditionDate", Sort.Direction.ASC);
-		repositoryItemReader.setSort(sorts);
-		repositoryItemReader.setPageSize(1000);
-		repositoryItemReader.setMaxItemCount(1000);
-        return repositoryItemReader;
-    }
-    
     public ItemReader<GovioMessageEntity> expiredScheduledDateMessageCursor(Status status) {
         JpaCursorItemReader<GovioMessageEntity> itemReader = new JpaCursorItemReader<>();
         itemReader.setQueryString("SELECT msg FROM GovioMessageEntity msg JOIN FETCH msg.govioServiceInstance srv WHERE msg.status = :status AND msg.scheduledExpeditionDate < :now");
