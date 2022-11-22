6138ae26d4ad17ac1d1ac9ad043ef54f9c896bdb
Lorenzo Nardi <nardi@link.it>
2022-11-22 21:16:27 +0100

    Perfezionata gestione dei batch

---
+++ batch/src/main/java/it/govio/batch/config/AbstractMessagesJobConfig.java
@@ -0,0 +1,95 @@
+package it.govio.batch.config;
+
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+import it.govio.batch.entity.GovioMessageEntity;
+import it.govio.batch.entity.GovioMessageEntity.Status;
+import it.govio.batch.repository.GovioMessagesRepository;
+import it.govio.batch.step.GovioMessageAbstractProcessor;
+
+
+@EnableBatchProcessing
+public abstract class AbstractMessagesJobConfig  {
+
+	@Autowired
+	protected JobBuilderFactory jobs;
+
+	@Autowired
+	protected StepBuilderFactory steps;
+	
+	@Autowired
+	protected GovioMessagesRepository govioMessagesRepository;
+	
+	@Autowired
+	protected EntityManager entityManager;
+	
+	protected TaskExecutor taskExecutor() {
+	    return new SimpleAsyncTaskExecutor("spring_batch_msgsender");
+	}
+	
+	protected AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncProcessor(GovioMessageAbstractProcessor itemProcessor) {
+	    AsyncItemProcessor<GovioMessageEntity, GovioMessageEntity> asyncItemProcessor = new AsyncItemProcessor<>();
+	    asyncItemProcessor.setTaskExecutor(taskExecutor());
+	    asyncItemProcessor.setDelegate(itemProcessor);
+	    return asyncItemProcessor;
+	}
+	
+	protected AsyncItemWriter<GovioMessageEntity> asyncMessageWriter(){
+		AsyncItemWriter<GovioMessageEntity> asyncItemWriter = new AsyncItemWriter<>();
+	    asyncItemWriter.setDelegate(messageWriter());
+	    return asyncItemWriter;
+	}
+	
+	protected RepositoryItemWriter<GovioMessageEntity> messageWriter() {
+        final RepositoryItemWriter<GovioMessageEntity> repositoryItemWriter = new RepositoryItemWriter<>();
+        repositoryItemWriter.setRepository(govioMessagesRepository);
+        repositoryItemWriter.setMethodName("save");
+        return repositoryItemWriter;
+    }
+	
+	protected ItemReader<GovioMessageEntity> expiredScheduledDateMessageCursor(Status[] statuses) {
+        JpaCursorItemReader<GovioMessageEntity> itemReader = new JpaCursorItemReader<>();
+        itemReader.setQueryString("SELECT msg FROM GovioMessageEntity msg JOIN FETCH msg.govioServiceInstance srv WHERE msg.status IN :statuses AND msg.scheduledExpeditionDate < :now");
+        itemReader.setEntityManagerFactory(entityManager.getEntityManagerFactory());
+        itemReader.setSaveState(true);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("statuses", Arrays.asList(statuses));
+        parameters.put("now", LocalDateTime.now());
+        itemReader.setParameterValues(parameters);
+        return itemReader;
+    }
+
+	@Value( "${govio.batch.verify-messages.delay-window:30}" )
+	protected int delayWindow;
+	protected ItemReader<GovioMessageEntity> expiredExpeditionDateMessageCursor(Status[] statuses) {
+        JpaCursorItemReader<GovioMessageEntity> itemReader = new JpaCursorItemReader<>();
+        itemReader.setQueryString("SELECT msg FROM GovioMessageEntity msg JOIN FETCH msg.govioServiceInstance srv WHERE msg.status IN :statuses AND msg.expeditionDate < :t0");
+        itemReader.setEntityManagerFactory(entityManager.getEntityManagerFactory());
+        itemReader.setSaveState(true);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("statuses", Arrays.asList(statuses));
+        parameters.put("t0", LocalDateTime.now().minusMinutes(delayWindow));
+        itemReader.setParameterValues(parameters);
+        return itemReader;
+    }
+
+}
---
+++ batch/src/test/java/it/govio/batch/test/batch/SendMessagesJobTest.java
@@ -0,0 +1,266 @@
+package it.govio.batch.test.batch;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import it.govio.batch.Application;
+import it.govio.batch.entity.GovioMessageEntity;
+import it.govio.batch.entity.GovioMessageEntity.Status;
+import it.govio.batch.entity.GovioServiceInstanceEntity;
+import it.govio.batch.repository.GovioMessagesRepository;
+import it.govio.batch.repository.GovioServiceInstancesRepository;
+import it.pagopa.io.v1.api.beans.CreatedMessage;
+import it.pagopa.io.v1.api.beans.FiscalCodePayload;
+import it.pagopa.io.v1.api.beans.LimitedProfile;
+import it.pagopa.io.v1.api.impl.ApiClient;
+
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+@EnableAutoConfiguration
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+public class SendMessagesJobTest {
+
+	@Mock
+	private RestTemplate restTemplate;
+
+	@Autowired
+	@InjectMocks
+	private ApiClient apiClient;
+
+	@Autowired
+	private GovioServiceInstancesRepository govioServiceInstancesRepository;
+
+	@Autowired
+	private GovioMessagesRepository govioMessagesRepository;
+	
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	@Qualifier(value = "SendMessagesJob")
+	private Job job;
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private JobRepository jobRepository;
+
+	private void initailizeJobLauncherTestUtils() throws Exception { 
+	    this.jobLauncherTestUtils = new JobLauncherTestUtils();
+	    this.jobLauncherTestUtils.setJobLauncher(jobLauncher);
+	    this.jobLauncherTestUtils.setJobRepository(jobRepository);
+	    this.jobLauncherTestUtils.setJob(job);
+	}
+	
+	/**
+	 * Database inizializzato con:
+	 * - 100 messaggi schedulati
+	 * - 100 messaggi in errore
+	 * - 100 messaggi spediti
+	 * - 100 messaggi 
+	 * @throws Exception
+	 */
+
+	@BeforeEach
+	void setUp(){
+		MockitoAnnotations.openMocks(this);
+		govioMessagesRepository.deleteAll();
+		
+		Optional<GovioServiceInstanceEntity> serviceInstanceEntity = govioServiceInstancesRepository.findById(1L);
+
+		for(int i=0; i<100; i++) {
+			GovioMessageEntity message = GovioMessageEntity.builder()
+					.govioServiceInstance(serviceInstanceEntity.get())
+					.markdown("Lorem Ipsum")
+					.subject("Subject")
+					.taxcode(String.format("%03d", i) + "AAA00A00A000A")
+					.scheduledExpeditionDate(LocalDateTime.now().minusDays(1))
+					.creationDate(LocalDateTime.now().minusDays(2))
+					.status(Status.SCHEDULED)
+					.build();
+			govioMessagesRepository.save(message);
+		}
+	}
+
+
+	@Test
+	public void sendMessagesOk() throws Exception {
+
+		// Caricamento messaggi da inviare
+
+		govioMessagesRepository.deleteAll();
+		
+		Random r = new Random();
+
+		Set<String> failedTaxCodes = new HashSet<>();
+
+		Mockito
+		.when(restTemplate.exchange(any(), eq(new ParameterizedTypeReference<LimitedProfile>() {})))
+		.thenAnswer(new Answer<ResponseEntity<LimitedProfile>>() {
+			@Override
+			public ResponseEntity<LimitedProfile> answer(InvocationOnMock invocation) throws Exception{
+				@SuppressWarnings("unchecked")
+				String fiscalCode = ((RequestEntity<FiscalCodePayload>) invocation.getArgument(0)).getBody().getFiscalCode();
+				int nextInt = r.nextInt(400);
+				Thread.sleep(nextInt+100);
+				
+				// Nel 10% dei casi lancio una eccezione, se il codice fiscale non è già stato oggetto di eccezioni.
+				if(nextInt < 40 && failedTaxCodes.contains(fiscalCode)) { 
+					failedTaxCodes.add(fiscalCode);
+					throw new IOException();
+				}
+				LimitedProfile profile = new LimitedProfile();
+				profile.setSenderAllowed(true);
+				return new ResponseEntity<LimitedProfile>(profile, HttpStatus.OK);
+			}
+		});
+
+
+		Mockito
+		.when(restTemplate.exchange(any(), eq(new ParameterizedTypeReference<CreatedMessage>() {})))
+		.thenAnswer(new Answer<ResponseEntity<CreatedMessage>>() {
+			@Override
+			public ResponseEntity<CreatedMessage> answer(InvocationOnMock invocation) throws InterruptedException{
+				Thread.sleep(r.nextInt(400)+100);
+				CreatedMessage createdMessage = new CreatedMessage();
+				createdMessage.setId(UUID.randomUUID().toString());
+				return new ResponseEntity<CreatedMessage>(createdMessage, HttpStatus.CREATED);
+			}
+		});
+		Mockito
+		.when(restTemplate.getUriTemplateHandler()).thenReturn(new DefaultUriBuilderFactory());
+
+		initailizeJobLauncherTestUtils();
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+		Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+
+		//Controllo che tutti i messaggi siano spediti con successo e aggiornati conseguentemente.
+
+		List<GovioMessageEntity> findAll = govioMessagesRepository.findAll();
+		for(GovioMessageEntity entity : findAll) {
+			Assert.assertNotNull(entity.getExpeditionDate());
+			Assert.assertNotNull(entity.getLastUpdateStatus());
+			Assert.assertEquals(Status.SENT, entity.getStatus());
+			Assert.assertNotNull(entity.getAppioMessageId());
+		}
+	}
+
+
+	@Test
+	public void sendMessagesFailure() throws Exception {
+
+		// Caricamento messaggi da inviare
+		Optional<GovioServiceInstanceEntity> serviceInstanceEntity = govioServiceInstancesRepository.findById(1L);
+
+		govioMessagesRepository.deleteAll();
+		
+		Random r = new Random();
+
+		for(int i=0; i<100; i++) {
+			GovioMessageEntity message = GovioMessageEntity.builder()
+					.govioServiceInstance(serviceInstanceEntity.get())
+					.markdown("Lorem Ipsum")
+					.subject("Subject")
+					.taxcode(String.format("%03d", i) + "AAA00A00A000A")
+					.scheduledExpeditionDate(LocalDateTime.now().minusDays(1))
+					.creationDate(LocalDateTime.now().minusDays(2))
+					.status(Status.SCHEDULED)
+					.build();
+			govioMessagesRepository.save(message);
+		}
+		
+		Set<String> failedTaxCodes = new HashSet<>();
+
+		Mockito
+		.when(restTemplate.exchange(any(), eq(new ParameterizedTypeReference<LimitedProfile>() {})))
+		.thenAnswer(new Answer<ResponseEntity<LimitedProfile>>() {
+			@Override
+			public ResponseEntity<LimitedProfile> answer(InvocationOnMock invocation) throws Exception{
+				@SuppressWarnings("unchecked")
+				String fiscalCode = ((RequestEntity<FiscalCodePayload>) invocation.getArgument(0)).getBody().getFiscalCode();
+				int nextInt = r.nextInt(400);
+				Thread.sleep(nextInt+100);
+				
+				// Nel 10% dei casi lancio una eccezione, se il codice fiscale non è già stato oggetto di eccezioni.
+				if(nextInt < 40 && failedTaxCodes.contains(fiscalCode)) { 
+					failedTaxCodes.add(fiscalCode);
+					throw new IOException();
+				}
+				LimitedProfile profile = new LimitedProfile();
+				profile.setSenderAllowed(true);
+				return new ResponseEntity<LimitedProfile>(profile, HttpStatus.OK);
+			}
+		});
+
+
+		Mockito
+		.when(restTemplate.exchange(any(), eq(new ParameterizedTypeReference<CreatedMessage>() {})))
+		.thenAnswer(new Answer<ResponseEntity<CreatedMessage>>() {
+			@Override
+			public ResponseEntity<CreatedMessage> answer(InvocationOnMock invocation) throws InterruptedException{
+				Thread.sleep(r.nextInt(400)+100);
+				CreatedMessage createdMessage = new CreatedMessage();
+				createdMessage.setId(UUID.randomUUID().toString());
+				return new ResponseEntity<CreatedMessage>(createdMessage, HttpStatus.CREATED);
+			}
+		});
+		Mockito
+		.when(restTemplate.getUriTemplateHandler()).thenReturn(new DefaultUriBuilderFactory());
+
+		initailizeJobLauncherTestUtils();
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+		Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+
+		//Controllo che tutti i messaggi siano spediti con successo e aggiornati conseguentemente.
+
+		List<GovioMessageEntity> findAll = govioMessagesRepository.findAll();
+		for(GovioMessageEntity entity : findAll) {
+			Assert.assertNotNull(entity.getExpeditionDate());
+			Assert.assertNotNull(entity.getLastUpdateStatus());
+			Assert.assertEquals(Status.SENT, entity.getStatus());
+			Assert.assertNotNull(entity.getAppioMessageId());
+		}
+	}
+}
