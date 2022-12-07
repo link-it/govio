32f219e591831035321ba12d3ca2572733aff4f6
Marco Flagella <marco.flagella@gmail.com>
2022-12-07 17:07:50 +0100

    Avanzamento testsuite

--- batch/src/main/java/it/govio/batch/entity/GovioFileMessageEntity.java
+++ batch/src/main/java/it/govio/batch/entity/GovioFileMessageEntity.java
@@ -2,8 +2,10 @@ package it.govio.batch.entity;
 
 
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +15,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import org.springframework.data.repository.cdi.Eager;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
@@ -30,18 +33,19 @@ import lombok.Setter;
 @Component
 @Table(name = "govio_files_messages")
 public class GovioFileMessageEntity {
-	
+
 	@Id
 	@SequenceGenerator(name="seq_govio_file_messages",sequenceName="seq_govio_file_messages", initialValue=1, allocationSize=1)
 	@GeneratedValue(strategy= GenerationType.SEQUENCE, generator="seq_govio_file_messages")
 	private Long id;
 
-	@OneToOne
+	@OneToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "id_govio_message", nullable = true)
 	private GovioMessageEntity govioMessage;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "id_govio_files", nullable = false)
+	
 	private GovioFileEntity govioFile;
 	
 	@Column(name = "line_record")
