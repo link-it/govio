dd48e539ec21491534f8b1ca764b1d78c4dd3bcc
fscarlato <f.scarlato@protonmail.com>
2022-12-20 19:03:11 +0100

    Prima implementazione Del servizio di file upload di govio

---
+++ api/src/main/java/it/govhub/govio/api/repository/TemplateEntityRepository.java
@@ -0,0 +1,9 @@
+package it.govhub.govio.api.repository;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import it.govhub.govio.api.entity.TemplateEntity;
+
+public interface TemplateEntityRepository extends JpaRepositoryImplementation<TemplateEntity, Long>{
+
+}
---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,33 @@
+-- Utenti
+
+INSERT INTO public.govhub_users (id, principal, full_name, email, enabled) VALUES (nextval('public.seq_govhub_users'), 'user_govio_sender', 'Antonio Messaggio', 'sender@govio.it', true);
+INSERT INTO public.govhub_users (id, principal, full_name, email, enabled) VALUES (nextval('public.seq_govhub_users'), 'user_govio_viewer', 'Giuseppe Venti', 'viewer@govio.it', true);
+
+
+-- Ruoli
+
+INSERT INTO public.govhub_roles (id, id_govhub_application, name) VALUES (nextval('public.seq_govhub_roles'), 2, 'govio_sender');
+INSERT INTO public.govhub_roles (id, id_govhub_application, name) VALUES (nextval('public.seq_govhub_roles'), 2, 'govio_viewer');
+
+
+-- user_govio_sender -> govio_sender
+
+INSERT INTO public.govhub_authorizations (id, id_govhub_user, id_govhub_role) VALUES (nextval('public.seq_govhub_authorizations'), (SELECT id FROM public.govhub_users WHERE principal='user_govio_sender'), (SELECT id FROM public.govhub_roles WHERE name='govio_sender'));
+
+
+-- user_govio_viewer -> govio_viewer
+
+INSERT INTO public.govhub_authorizations (id, id_govhub_user, id_govhub_role) VALUES (nextval('public.seq_govhub_authorizations'), (SELECT id FROM public.govhub_users WHERE principal='user_govio_viewer'), (SELECT id FROM public.govhub_roles WHERE name='govio_viewer'));
+
+-- Creo un template dummy
+
+INSERT INTO public.govio_templates (id) VALUES (nextval('public.seq_govio_templates'));
+
+
+-- Creo anche una service instance
+
+INSERT INTO public.govio_service_instances(id, id_govhub_service, id_govhub_organization, id_govio_template) VALUES (nextval('public.seq_govio_service_instances'), 1, 1, 1);
+
+-- Service instance senza template
+
+INSERT INTO public.govio_service_instances(id, id_govhub_service, id_govhub_organization, id_govio_template) VALUES (nextval('public.seq_govio_service_instances'), 1, 2, null);
