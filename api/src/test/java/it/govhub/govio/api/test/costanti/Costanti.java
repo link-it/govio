a6023af44a98503839343543f495ae706c9309d4
Giuliano Pintori <pintori@link.it>
2022-12-21 18:20:31 +0100

    Issue #3 Aggiunto primo test con invio del csv via MockMvc e richiesta MockMvcRequestBuilders.multipart.

---
+++ api/src/test/java/it/govhub/govio/api/test/costanti/Costanti.java
@@ -0,0 +1,7 @@
+package it.govhub.govio.api.test.costanti;
+
+public class Costanti {
+
+	public static final String PARAMETRO_SERVICE_ID = "service_id";
+	public static final String PARAMETRO_ORGANIZATION_ID = "organization_id";
+}
