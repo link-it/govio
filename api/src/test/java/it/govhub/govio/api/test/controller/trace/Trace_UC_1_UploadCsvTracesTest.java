9566fa93b0eca24f074fff7e43a65d827687689c
Giuliano Pintori <pintori@link.it>
2022-12-21 18:20:31 +0100

    Issue #3 Aggiunto primo test con invio del csv via MockMvc e richiesta MockMvcRequestBuilders.multipart.

--- api/src/test/java/it/govhub/govio/api/test/controller/trace/Trace_UC_1_UploadCsvTracesTest.java
+++ api/src/test/java/it/govhub/govio/api/test/controller/trace/Trace_UC_1_UploadCsvTracesTest.java
@@ -1,15 +1,32 @@
 package it.govhub.govio.api.test.controller.trace;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+
+import javax.json.Json;
+import javax.json.JsonReader;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import it.govhub.govio.api.Application;
+import it.govhub.govio.api.test.costanti.Costanti;
 import it.govhub.govio.api.test.utils.UserAuthProfilesUtils;
 
 
@@ -27,7 +44,26 @@ class Trace_UC_1_UploadCsvTracesTest {
 	private UserAuthProfilesUtils userAuthProfilesUtils;
 	
 	@Test
-	void UC_1_01_RunOk() throws Exception {
+	void UC_1_01_UploadCsvFileOk() throws Exception {
+		String name = "user-file";
+		String fileName = "test1.csv";
+		String contentType = "text/csv";
+		String content = "test-data";
+		MockMultipartFile mockMultipartFile = new MockMultipartFile(name, fileName, contentType, content.getBytes());
 		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add(Costanti.PARAMETRO_SERVICE_ID, "1");
+		params.add(Costanti.PARAMETRO_ORGANIZATION_ID, "1");
+		
+		MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.multipart(HttpMethod.POST, new URI("/files"))
+				.file(mockMultipartFile)
+				.params(params)
+				.characterEncoding("UTF-8")
+				.with(this.userAuthProfilesUtils.utenzaAdmin())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		//JsonReader reader = Json.createReader(new ByteArrayInputStream(result.getResponse().getContentAsByteArray()));
 	}
 }
