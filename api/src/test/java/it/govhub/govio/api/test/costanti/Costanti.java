26100049a7620ebaa65692ebd13f80d2febcdde6
Giuliano Pintori <pintori@link.it>
2022-12-28 16:39:11 +0100

    Issue #3 Aggiunt test di invio tracciati cv.

--- api/src/test/java/it/govhub/govio/api/test/costanti/Costanti.java
+++ api/src/test/java/it/govhub/govio/api/test/costanti/Costanti.java
@@ -5,13 +5,19 @@ import it.govhub.govregistry.commons.entity.ServiceEntity;
 import it.govhub.govregistry.commons.utils.LimitOffsetPageRequest;
 
 public class Costanti {
+	
+	public final static char[] MULTIPART_CHARS = "-_1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	public static final String PART_NAME_FILE = "file";
+	
 
 	public static final String PARAMETRO_SERVICE_ID = "service_id";
 	public static final String PARAMETRO_ORGANIZATION_ID = "organization_id";
 	public static final String TEXT_CSV_CONTENT_TYPE = "text/csv";
 	
+	public static final String TAX_CODE_ENTE_CREDITORE = "12345678901";
+	public static final String LEGALNAME_ENTE_CREDITORE = "Ente Creditore";
+	
 	public static final String TAX_CODE_ENTE_CREDITORE_2 = "12345678902";
-	public static final String LEGALNAME_ENTE_CREDITORE_2 = "Ente Creditore 2";
 	
 	public static final String USERS_QUERY_PARAM_LIMIT = "limit";
 	public static final String USERS_QUERY_PARAM_OFFSET = "offset";
@@ -29,12 +35,14 @@ public class Costanti {
 	public static final String SERVICE_NAME_SERVIZIO_GENERICO = "Servizio Generico";
 	public static final String SERVICE_DESCRIPTION_SERVIZIO_GENERICO = "Esempio di servizio";
 	
+	public static final String SERVICE_NAME_TARI = "TARI";
+	
 	/* ORGANIZATIONS */
 	
-	public static OrganizationEntity getEnteCreditore2() {
+	public static OrganizationEntity getEnteCreditore() {
 		return OrganizationEntity.builder()
-				.taxCode(Costanti.TAX_CODE_ENTE_CREDITORE_2)
-				.legalName(Costanti.LEGALNAME_ENTE_CREDITORE_2)
+				.taxCode(Costanti.TAX_CODE_ENTE_CREDITORE)
+				.legalName(Costanti.LEGALNAME_ENTE_CREDITORE)
 				.build();
 	}
 	/* SERVICES */
