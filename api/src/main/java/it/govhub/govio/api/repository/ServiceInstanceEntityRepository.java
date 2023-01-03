2a939e162fc3a706627941e336b63fc123a877fb
fscarlato <f.scarlato@protonmail.com>
2023-01-03 18:13:07 +0100

    wip

--- api/src/main/java/it/govhub/govio/api/repository/ServiceInstanceEntityRepository.java
+++ api/src/main/java/it/govhub/govio/api/repository/ServiceInstanceEntityRepository.java
@@ -2,11 +2,14 @@ package it.govhub.govio.api.repository;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 import it.govhub.govio.api.entity.ServiceInstanceEntity;
 
 public interface ServiceInstanceEntityRepository extends JpaRepositoryImplementation<ServiceInstanceEntity, Long> {
 	
+	@Transactional
 	public Optional<ServiceInstanceEntity> findByService_IdAndOrganization_Id(Long serviceId, Long organizationId);
 }
