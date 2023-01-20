06acdd1e81b2e3ab26cdd6407433c34dcca2e15b
fscarlato <f.scarlato@protonmail.com>
2023-01-20 17:24:44 +0100

    Customizzato l'object mapper con serializationInclusion(Include.NON_EMPTY) in modo da non serializzare le liste vuote e non far comparire i link hateoas vuoti

--- api/src/main/java/it/govhub/govio/api/Application.java
+++ api/src/main/java/it/govhub/govio/api/Application.java
@@ -18,6 +18,8 @@ import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import it.govhub.govregistry.commons.config.CommonsExportedBeans;
 import it.govhub.govregistry.commons.config.TimeZoneConfigurer;
 import it.govhub.govregistry.commons.exception.handlers.RequestRejectedExceptionHandler;
@@ -59,6 +61,7 @@ public class Application extends SpringBootServletInitializer {
 	public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
 		return builder ->  builder.
 				timeZone(this.timeZone).
+				serializationInclusion(Include.NON_EMPTY).
 				serializerByType(Base64String.class, new Base64StringSerializer());
 	}
 
