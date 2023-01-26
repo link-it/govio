77ea0fcb24aa1bb7027572eb8fd4d4aa46828f6d
Lorenzo Nardi <nardi@link.it>
2023-01-26 13:27:19 +0100

    Corretta definizione dell'id in GovioFileEntity

--- api/src/main/java/it/govhub/govio/api/entity/GovioFileEntity.java
+++ api/src/main/java/it/govhub/govio/api/entity/GovioFileEntity.java
@@ -11,9 +11,11 @@ import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import it.govhub.govregistry.commons.entity.UserEntity;
@@ -35,7 +37,9 @@ public class GovioFileEntity {
 
 	public enum Status {CREATED, PROCESSING, PROCESSED}
 
-	@Id @GeneratedValue
+	@Id 
+	@SequenceGenerator(name="seq_govio_files",sequenceName="seq_govio_files", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy= GenerationType.SEQUENCE, generator="seq_govio_files")
 	private Long id;
 
 	@ManyToOne
