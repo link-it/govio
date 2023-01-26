97ac045d786ea4a8f82b07ff96da79bc6e88fcbd
Lorenzo Nardi <nardi@link.it>
2023-01-26 16:32:21 +0100

    Corretto formato del due_date nell messaggio verso IO anche nei test

--- batch/src/test/java/it/govio/batch/test/step/UC2_NewMessageServiceTest.java
+++ batch/src/test/java/it/govio/batch/test/step/UC2_NewMessageServiceTest.java
@@ -113,7 +113,7 @@ class UC2_NewMessageServiceTest {
 		content.setMarkdown(govioMessageEntity.getMarkdown());
 		content.setSubject(govioMessageEntity.getSubject());
 		if(govioMessageEntity.getDueDate() != null)
-		content.setDueDate(govioMessageEntity.getDueDate().atOffset(ZoneOffset.UTC).toString());
+		content.setDueDate(NewMessageProcessor.dtf.format(govioMessageEntity.getDueDate().atOffset(ZoneOffset.UTC)));
 		if(govioMessageEntity.getNoticeNumber() != null) {
 			Assert.assertNotNull(govioMessageEntity.getAmount());
 			PaymentData paymentData = new PaymentData();
