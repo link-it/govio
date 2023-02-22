e4118acdb979c9058087eb9497813d6796b0ea50
fscarlato <f.scarlato@protonmail.com>
2023-02-22 13:11:18 +0100

    fix compilazione tests batch

--- batch/src/main/java/it/govio/batch/entity/FileMessageStats.java
+++ batch/src/main/java/it/govio/batch/entity/FileMessageStats.java
@@ -4,6 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -11,13 +15,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+/*@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "govio_file_message_stats")
+@Table(name = "govio_file_message_stats")*/
 public class FileMessageStats {
+	
+	/*@Id
+	@SequenceGenerator(name="seq_govio_file_message_stats",sequenceName="seq_govio_file_message_stats", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy= GenerationType.SEQUENCE, generator="seq_govio_file_message_stats")
+	private Long id;
 	
 	GovioFileEntity file;
 	
@@ -25,6 +34,6 @@ public class FileMessageStats {
 	@Enumerated(EnumType.STRING)
 	GovioMessageEntity.Status status;
 	
-	Long count;
+	Long count;*/
 	
 }
