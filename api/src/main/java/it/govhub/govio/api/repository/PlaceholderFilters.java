492b6cc88203fe2e83d909e3addaf315f65c96af
fscarlato <f.scarlato@protonmail.com>
2023-02-24 11:17:29 +0100

    aggiunto file mancante

---
+++ api/src/main/java/it/govhub/govio/api/repository/PlaceholderFilters.java
@@ -0,0 +1,30 @@
+package it.govhub.govio.api.repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import it.govhub.govio.api.entity.GovioPlaceholderEntity;
+import it.govhub.govio.api.entity.GovioPlaceholderEntity_;
+
+public class PlaceholderFilters {
+	
+	public static Specification<GovioPlaceholderEntity> empty() {
+		return (Root<GovioPlaceholderEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> null;
+	}
+	
+	
+	public static Specification<GovioPlaceholderEntity> likeName(String name) {
+		return (Root<GovioPlaceholderEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> 
+			cb.like(cb.upper(root.get(GovioPlaceholderEntity_.name)), "%"+name.toUpperCase()+"%");
+	}
+	
+	
+	public static Specification<GovioPlaceholderEntity> likeDescription(String description) {
+		return (Root<GovioPlaceholderEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> 
+			cb.like(cb.upper(root.get(GovioPlaceholderEntity_.description)), "%"+description.toUpperCase()+"%");
+	}
+	
+}
