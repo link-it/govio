4b9780b6ebc6374409a58e756539a3f6a1d263bc
Giuliano Pintori <pintori@link.it>
2023-03-22 14:32:41 +0100

    Aggiunti test per la lettura del dettaglio di un service-instance.

---
+++ api/src/test/java/it/govhub/govio/api/test/controller/serviceInstance/ServiceInstance_UC_2_GetServiceInstanceTest.java
@@ -0,0 +1,118 @@
+package it.govhub.govio.api.test.controller.serviceInstance;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.ByteArrayInputStream;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import it.govhub.govio.api.Application;
+import it.govhub.govio.api.repository.TemplateRepository;
+import it.govhub.govio.api.test.utils.UserAuthProfilesUtils;
+import it.govhub.govregistry.readops.api.repository.ReadOrganizationRepository;
+import it.govhub.govregistry.readops.api.repository.ReadServiceRepository;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+@DisplayName("Test di lettura Service Instance")
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
+
+class ServiceInstance_UC_2_GetServiceInstanceTest {
+
+	private static final String SERVICE_INSTANCES_BASE_PATH = "/v1/service-instances";
+	private static final String SERVICE_INSTANCES_BASE_PATH_DETAIL_ID = SERVICE_INSTANCES_BASE_PATH + "/{id}";
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private UserAuthProfilesUtils userAuthProfilesUtils;
+	
+	@Autowired
+	ReadServiceRepository serviceRepository;
+	
+	@Autowired
+	ReadOrganizationRepository organizationRepository;
+	
+	@Autowired
+	TemplateRepository templateRepository;
+	
+	@Test
+	void UC_2_01_GetServiceInstanceOk() throws Exception {
+		MvcResult result = this.mockMvc.perform(get(SERVICE_INSTANCES_BASE_PATH)
+				.with(this.userAuthProfilesUtils.utenzaAdmin())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		JsonReader reader = Json.createReader(new ByteArrayInputStream(result.getResponse().getContentAsByteArray()));
+		JsonObject siList = reader.readObject();
+		
+		// Controlli sugli items
+		JsonArray items = siList.getJsonArray("items");
+		assertEquals(3, items.size());
+		
+		JsonObject item1 = items.getJsonObject(0); 
+		int idService1 = item1.getInt("id");
+		
+		result = this.mockMvc.perform(get(SERVICE_INSTANCES_BASE_PATH_DETAIL_ID,idService1)
+				.with(this.userAuthProfilesUtils.utenzaAdmin())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		reader = Json.createReader(new ByteArrayInputStream(result.getResponse().getContentAsByteArray()));
+		JsonObject item = reader.readObject();
+		
+		assertEquals("17886617e07d47e8b1ba314f2f1e3053", item.getString("apiKey"));
+		
+	}
+	
+	@Test
+	void UC_2_02_GetServiceInstance_NotFound() throws Exception {
+		int idService1 = 10000;
+		
+		this.mockMvc.perform(get(SERVICE_INSTANCES_BASE_PATH_DETAIL_ID,idService1)
+				.with(this.userAuthProfilesUtils.utenzaAdmin())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andExpect(jsonPath("$.status", is(404)))
+				.andExpect(jsonPath("$.title", is("Not Found")))
+				.andExpect(jsonPath("$.type").isString())
+				.andExpect(jsonPath("$.detail").isString())
+				.andReturn();
+	}
+	
+	@Test	
+	void UC_2_03_GetServiceInstance_InvalidId() throws Exception {
+		String idService1 = "XXX";
+		
+		this.mockMvc.perform(get(SERVICE_INSTANCES_BASE_PATH_DETAIL_ID,idService1)
+				.with(this.userAuthProfilesUtils.utenzaAdmin())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.status", is(400)))
+				.andExpect(jsonPath("$.title", is("Bad Request")))
+				.andExpect(jsonPath("$.type").isString())
+				.andExpect(jsonPath("$.detail").isString())
+				.andReturn();
+	}
+}
