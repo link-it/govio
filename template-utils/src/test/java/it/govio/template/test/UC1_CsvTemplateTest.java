8d5e6044cff87f7a9e4a762db468c8d89f29dd20
Lorenzo Nardi <nardi@link.it>
2023-04-14 12:47:11 +0200

    Corretti nullpointer in caso di valori blank nel csv

--- template-utils/src/test/java/it/govio/template/test/UC1_CsvTemplateTest.java
+++ template-utils/src/test/java/it/govio/template/test/UC1_CsvTemplateTest.java
@@ -35,9 +35,7 @@ import org.junit.jupiter.api.Test;
 
 import freemarker.template.TemplateException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class UC1_CsvTemplateTest {
 	
@@ -301,7 +299,6 @@ class UC1_CsvTemplateTest {
 
 	}
 
-
 	@Test
 	@DisplayName("template con placeholder stringa senza pattern")
 	void UC_1_15_PlaceholderStringaSenzaPattern() throws IOException, TemplateException, TemplateValidationException{
@@ -508,30 +505,24 @@ class UC1_CsvTemplateTest {
 		assertEquals("messaggio semplice con un placeholder con date time :  03/12/2007 10:15, 03/12/2007, lunedì 03 dicembre 2007, 10:15, lunedì 03 dicembre 2007 alle ore 10:15",messaggio.getMarkdown());
 	}
 
-
 	@Test
-	@DisplayName("Messaggio senza due date, senza payment, senza placeholder")
-	void UC_1_X_OnlyTaxcodeExpeditionDate() throws IOException, TemplateException, TemplateValidationException{
+	@DisplayName("template con payment senza payee")
+	void UC_1_22_PaymentSenzaPayee() throws IOException, TemplateException, TemplateValidationException{
 		List<Placeholder> placeholders = new ArrayList<> ();
-		Placeholder p = Placeholder.builder()
-				.name("nome")
-				.type(Type.STRING)
-				.position(1)
-				.mandatory(true)
-				.build();
-		placeholders.add(p);
 
 		Template template = Template
 				.builder()
 				.hasDueDate(false)
-				.hasPayment(false)
-				.messageBody("Lorem ipsum dolor set amet ${nome}. Lorem ipsum dolor set amet. Lorem ipsum dolor set amet.")
+				.hasPayment(true)
+				.messageBody("Message deve essere di almento 80 caratteri. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet aliquam tincidunt, nunc mauris aliquam erat, vel")
 				.subject("hello, il subject deve essere almeno dieci caratteri")
 				.placeholders(placeholders)
 				.build();
-		CsvTemplateApplier templateApplier = TemplateApplierFactory.buildCSVTemplateApplier(template);
 
-		Message messaggio = templateApplier.buildMessage("RSSMRO00A00A000A,2023-01-01T12:00:00,Mario");
-		assertEquals("Lorem ipsum dolor set amet Mario. Lorem ipsum dolor set amet. Lorem ipsum dolor set amet.",messaggio.getMarkdown());
+		CsvTemplateApplier templateApplier = TemplateApplierFactory.buildCSVTemplateApplier(template);
+		Message messaggio = templateApplier.buildMessage("RSSMRO00A00A000A,2023-01-01T12:00:00,300000000000000000,999,,,");
+
+		assertNull(messaggio.getDueDate());
+		assertNull(messaggio.getPayee());
 	}
 }
